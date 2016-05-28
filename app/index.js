1fa0f6b3a3bd92712d841f736fd7ed0796161916
hzn <huzhennan2015@163.com>
2016-05-28 17:57:58 +0800

    add hello component

--- app/index.js
+++ app/index.js
@@ -1,2 +1,13 @@
 
-console.log("hello from index.js")
+var React = require('react')
+var ReactDOM = require('react-dom')
+
+var HelloWorld = React.createClass({
+  render: function() {
+    return (
+      <div>Hello World!</div>
+    )
+  }
+})
+
+ReactDOM.render(<HelloWorld/>, document.getElementById('app'))
