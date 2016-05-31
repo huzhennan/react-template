47feb9ddbf5bf5edb253731d22b39d1b922f1fc6
hzn <huzhennan2015@163.com>
2016-05-31 17:34:01 +0800

    use es2015

--- app/index.js
+++ app/index.js
@@ -1,8 +1,8 @@
 
-var React = require('react')
-var ReactDOM = require('react-dom')
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-var HelloWorld = React.createClass({
+const HelloWorld = React.createClass({
   render: function() {
     return (
       <div>Hello World!</div>
